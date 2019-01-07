7bda889ce095aea9e52eff076ed823055fb80bef
YenChin <kugh30122@yahoo.com.tw>
2019-01-07 16:49:17 +0800

    add function[]

--- main.c
+++ main.c
@@ -19,27 +19,30 @@ int main()
 
     printf("   學生                           成績                                             總和平均\n");
     printf("    A");
+
+    student_score_A(A);
     for(i=1;i<=10;i++)
     {
-    printf("     %d",A[i]);
     standard+=A[i];
     }
    standard=ST(standard);
     printf("    B");
+        student_score_B(B);
     for(i=1;i<=10;i++)
     {
-    printf("     %d",B[i]);
-    standard+=B[i];
+     standard+=B[i];
     }
     standard=ST(standard);
     printf("    C");
+
+    student_score_C(C);
     for(i=1;i<=10;i++)
     {
-    printf("     %d",C[i]);
     standard+=C[i];
     }
     standard=ST(standard);
     printf("*********************\n");
+
     for(i=1;i<=10;i++)
     {
     printf("   %p",&A[i]);
@@ -67,4 +70,32 @@ int ST(int standard)
     standard=standard/10;
     printf("            %d\n",standard);
   return 0;
+
  }
+
+ void student_score_A(int A[])
+ {
+     int i;
+      for(i=1;i<=10;i++)
+    {
+         printf("     %d",A[i]);
+    }
+ }
+ void student_score_B(int B[])
+ {
+     int i;
+     for(i=1;i<=10;i++)
+    {
+         printf("     %d",B[i]);
+    }
+ }
+ void student_score_C(int C[])
+ {
+     int i;
+     for(i=1;i<=10;i++)
+    {
+         printf("     %d", C[i]);
+    }
+ }
+
+
